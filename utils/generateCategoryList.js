2eb8a6db5933dd5b57303b4bb4f3b7979ccba183
pendant_k <behind8486@gmail.com>
2022-05-19 15:22:11 +0900

    Add utils

---
+++ utils/generateCategoryList.js
@@ -0,0 +1,14 @@
+// var fs = require("fs");
+// var array = fs
+//     .readFileSync(
+//         "/Users/pendant.k/coding/javascript/capstoneF/capstone-Team-F/utils/kor_keywords.txt"
+//     )
+//     .toString()
+//     .split("\n");
+// console.log(array);
+
+function random() {
+    return Math.floor(Math.random() * 99);
+}
+
+console.log(random());
